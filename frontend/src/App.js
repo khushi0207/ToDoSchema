a86802d11d5e0d540786750c9d4d6e3668fdf2f3
khushi0207 <127587174+khushi0207@users.noreply.github.com>
2024-08-12 00:24:05 +0530

    commit new changes..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import Input from './components/Input';
+  import logo from './logo.svg';
+  import './App.css';
+  import Input from './components/Input';
+  import { useState } from "react";
 
-function App() {
-  return (
-<Input></Input>
-  );
-}
+  function App() {
+    const [title, setTitle] = useState("")
+    const [desc, setDesc] = useState("") 
 
-export default App;
+    function handleChange(event) {
+      if(event.target.name === "Title"){
+        setTitle(event.target.value)
+      }else if(event.target.name === "Description"){
+        setDesc(event.target.value)
+      }
+      
+    }
+
+    function handleUpdate() {
+      
+    }
+
+    function handleDelete(){
+      
+    }
+
+    function hancleClick(){
+      console.log(`Title: ${title}, and Description: ${desc}`)
+    }
+
+    return (
+      <div>
+        <ul>
+          <li>
+          <Input label="Title" onClick={handleUpdate} name="Title" onChange={handleChange} value={title}/>
+          </li>
+          <li>
+          <Input label="Description" onClick={handleDelete} name="Description" onChange={handleChange} value={desc}/>
+
+          </li>
+        </ul>
+        <button onClick={hancleClick}>Submit</button>
+      </div>
+    );
+  }
+
+  export default App;
--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,13 +1,22 @@
 
-function Input(label){
-    let arr = ["Title","description"];
-    let arr1 =arr.map((i) => <Input label={i}></Input>)
-  
-    return (
-        <div>
-{arr1}
-      </div>
-     );
-  }
+
+function Input(props){
+
+  return(
+    <div>
+      <ul>
+        <li>
+        <label>Enter{props.label}: </label>
+        </li>
+        <dd>
+        <input type="text" onChange={props.onChange} name={props.label} placeholder={`Enter ${props.label}: `} value={props.value} />
+        </dd>
+      </ul>
+      <button  name="Update" onClick={props.onChange} style={{backgroundColor:'plum',margin: '10px', borderRadius:'10px', cursor:'pointer',display:'inline-flex'}}>Update</button>
+      <button  name="Delete" onClick={props.onChange} style={{backgroundColor:'aqua',margin: '10px', borderRadius:'10px', cursor:'pointer', display:'inline-flex' }} >Delete</button>
+    </div>
+  );
+
+}
 
   export default Input;
